d188969b9c4a05b2cdca194c03518443425f6207
MA <62040646+MartyAlien@users.noreply.github.com>
2020-07-27 18:56:17 +0800

    更改了短信接口的accessKey
    如果需要短信验证码接口，前往阿里云官网获取

--- src/com/program/check/StaticPeram.java
+++ src/com/program/check/StaticPeram.java
@@ -12,11 +12,11 @@ public class StaticPeram {
 	final static String product = "Dysmsapi";// 短信API产品名称（短信产品名固定，无需修改）
 	final static String domain = "dysmsapi.aliyuncs.com";// 短信API产品域名（接口地址固定，无需修改）
 	// 替换成你的AK (产品密)
-	final static String accessKeyId = "LTAI4G7ioUh7GdP5yf5Apmh5";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
-	final static String accessKeySecret = "9IOOdebw0ug9qq8arbM4CazZWE1D4u";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
+	final static String accessKeyId = "LT****************h5";// 你的accessKeyId,填你自己的 上文配置所得  自行配置
+	final static String accessKeySecret = "9I******************4u";// 你的accessKeySecret,填你自己的 上文配置所得 自行配置
 	// 必填:短信签名-可在短信控制台中找到
 	final static String SignName = "物业收费管理系统";// 阿里云配置你自己的短信签名填入
 	// 必填:短信模板-可在短信控制台中找到
-	final static String TemplateCode[] = {"SMS_196618708","SMS_196653767"}; // 阿里云配置你自己的短信模板填入
+	final static String TemplateCode[] = {"SMS_19661**08","SMS_19665**67"}; // 阿里云配置你自己的短信模板填入
 	// 0 注册模板  1 忘记模板 2 修改模板
 }
